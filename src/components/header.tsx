aea59eaceeec5b7de36163c78a539c9fee6e6552
savicaleksa <aleksa.savic91@gmail.com>
2025-11-08 19:16:36 +0100

    add header, install recharts

---
+++ src/components/header.tsx
@@ -0,0 +1,37 @@
+const Header = () => {
+  return (
+    <header className="pb-18 pt-4 rounded-lg bg-gray-900 m-2">
+      <nav className="container flex justify-between gap-4 items-center relative mb-12">
+        <a
+          href="https://www.jetbrains.com"
+          className="p-2 bg-gray-100 rounded-lg"
+        >
+          <img
+            src="https://resources.jetbrains.com/storage/products/company/brand/logos/jetbrains.png"
+            alt="JetBrains"
+            className="sm:h-8 h-5 w-auto"
+          />
+        </a>
+        <a
+          className="text-sm sm:text-base font-semibold underline underline-offset-2 hover:no-underline"
+          href="https://savicaleksa.com"
+          target="_blank"
+        >
+          PORTFOLIO
+        </a>
+      </nav>
+      <div className="container">
+        <div className="text-center space-y-4 lg:space-y-8">
+          <h1 className="text-3xl font-bold sm:text-4xl lg:text-5xl">
+            Survey Visualizer Integration
+          </h1>
+          <p className="sm:text-lg lg:text-xl">
+            Interactive visualization of data from the Open Trivia DB
+          </p>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
