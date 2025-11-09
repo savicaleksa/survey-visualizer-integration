bd42d83f664cb03d9337edda0bcff8e6ce61cc54
savicaleksa <aleksa.savic91@gmail.com>
2025-11-09 05:19:31 +0100

    add view source buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,10 +18,10 @@ const Header = () => {
         </a>
         <a
           className="text-sm sm:text-base font-semibold underline underline-offset-2 hover:no-underline"
-          href="https://savicaleksa.com"
+          href="https://github.com/savicaleksa/survey-visualizer-integration"
           target="_blank"
         >
-          PORTFOLIO
+          GITHUB SOURCE
         </a>
       </nav>
       <div className="container">
@@ -32,7 +32,15 @@ const Header = () => {
           <p className="sm:text-lg lg:text-xl">
             Interactive visualization of data from the Open Trivia DB
           </p>
-          <LoadDataButton />
+          <div className="flex flex-col items-center gap-4">
+            <LoadDataButton />
+            <a
+              href="https://github.com/savicaleksa/survey-visualizer-integration"
+              className="py-2 px-4 bg-pink-200 disabled:opacity-50 disabled:pointer-events-none text-pink-950 hover:bg-pink-100 font-semibold cursor-pointer hover: rounded-lg"
+            >
+              View source code
+            </a>
+          </div>
         </div>
       </div>
     </header>
